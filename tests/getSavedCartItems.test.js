a87d1785b208a663a4ff22b60393d31344905ddd
Leo Araujo <leonardo_02452@hotmail.com>
2022-02-03 19:38:01 -0300

    Implementados os testes da função getSavedCartItems (Req 11)

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,12 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it ('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado.', async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+ });
+  it (`Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o 'cartItems' como parâmetro.`, async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toBeCalledWith('cartItems');
+ });
 });
