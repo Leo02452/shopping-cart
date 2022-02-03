7fc523b5e1606e249536a6f6814c9457d6b0018c
Leo Araujo <leonardo_02452@hotmail.com>
2022-02-03 15:41:55 -0300

    Implementados os testes da função fetchItem (Req 9)

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,9 +1,13 @@
 const fetchItem = async (id) => {
-  const url = `https://api.mercadolibre.com/items/${id}`;
-  const result = await fetch(url);
-  const data = await result.json();
-
-  return data;
+  try {
+    const url = `https://api.mercadolibre.com/items/${id}`;
+    const result = await fetch(url);
+    const data = await result.json();
+  
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
